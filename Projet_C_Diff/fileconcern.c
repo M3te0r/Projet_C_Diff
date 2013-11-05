5b109f33b093d302ec623db55b6edf3665d83346
M3te0r <mat.pequin@gmail.com>
2013-11-06 00:43:58 +0100

    help

--- Projet_C_Diff/fileconcern.c
+++ Projet_C_Diff/fileconcern.c
@@ -1,1 +1,9 @@
-int checkFiles;
+//Options fonctions of the diff command
+
+void help_option()
+{
+	printf("Usage: diff [OPTION]... FICHIERS");
+	printf("Comparer les fichiers ligne par ligne.");
+
+	//suite des détails d'options
+}
