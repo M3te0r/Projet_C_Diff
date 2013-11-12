d680f6cb908ce80434e74df7b1ca584b3a21c2be
guillaumeBlondeau <guillome6@hotmail.fr>
2013-11-12 09:55:32 +0100

    Fonctions pour algo principal

--- Projet_C_Diff/fileconcern.c
+++ Projet_C_Diff/fileconcern.c
@@ -131,4 +131,210 @@ char** fileToTabs(char* file, int lines)
 
 }
 
+// Renvoie le nombre de lignes du fichier
+int compteLigneFichier(const char *nomFichier)
+{
+	FILE *fichier = NULL;
+	int caractereActuel = 0, nb = 1;
+	fichier = fopen(nomFichier, "r");
+	if (fichier != NULL)
+	{
+		// Nombre de lignes du fichier
+		while (caractereActuel != EOF)
+		{
+			caractereActuel = fgetc(fichier);
+			if (caractereActuel == '\n')
+			{
+				nb++;
+			}
+		}
+	}
+	else
+	{
+		nb = 0;
+	}
+	fclose(fichier);
+	return nb;
+}
 
+// Modifie le tableau existant et ajoute les caractères d'une ligne dedans
+void ajouterATableauLigneFichier(const char *nomFichier, char *tabLigne, int numLigne, int tailleTableau)
+{
+	FILE *fichier = NULL;
+	int caractereActuel = 0, i = 0;
+	long position = 0;
+	fichier = fopen(nomFichier, "r");
+    position = retourLigneCurseur(nomFichier, numLigne);
+    fseek(fichier, position, SEEK_SET);
+	if (fichier != NULL)
+	{
+		for(i=0; i<tailleTableau; i++)
+		{
+		    caractereActuel = fgetc(fichier);
+			tabLigne[i] = caractereActuel;
+		}
+	}
+	fclose(fichier);
+}
+
+// Retourne la position du curseur au début de la ligne voulue
+int retourLigneCurseur(const char *nomFichier, int numLigne)
+{
+	FILE *fichier = NULL;
+	int caractereActuel = 0, nb = 0, nombreBackSlashN = 0;
+	fichier = fopen(nomFichier, "r");
+	if (fichier != NULL)
+	{
+        while (nombreBackSlashN != numLigne)
+        {
+            caractereActuel = fgetc(fichier);
+            if (caractereActuel == '\n' || caractereActuel == EOF)
+            {
+                nombreBackSlashN++;
+            }
+        }
+        nb = ftell(fichier);
+	}
+	else
+	{
+		nb = 0;
+	}
+	fclose(fichier);
+	return nb;
+}
+
+// Renvoie le nombre de caractères pour une ligne
+int nombreCaractereLigne(const char *nomFichier, int numLigne)
+{
+	FILE *fichier = NULL;
+	int caractereActuel = 0, nb = 0;
+	long position = 0;
+	fichier = fopen(nomFichier, "r");
+    position = retourLigneCurseur(nomFichier, numLigne);
+    fseek(fichier, position, SEEK_SET);
+	if (fichier != NULL)
+	{
+        while(caractereActuel != '\n')
+        {
+            if (caractereActuel == '\n' || caractereActuel == EOF)
+            {
+                break;
+            }
+            caractereActuel = fgetc(fichier);
+            nb++;
+        }
+	}
+	else
+	{
+		nb = 1;
+	}
+	fclose(fichier);
+	return nb-1;
+}
+
+// Affichage de la ligne voulue
+void afficheLigne(const char *nomFichier, int numLigne)
+{
+	FILE *fichier = NULL;
+	int caractereActuel = 0, i = 0;
+	long position = 0;
+	fichier = fopen(nomFichier, "r");
+    position = retourLigneCurseur(nomFichier, numLigne);
+    fseek(fichier, position, SEEK_SET);
+	if (fichier != NULL)
+	{
+		while (caractereActuel != '\n' || caractereActuel == EOF)
+        {
+            if (caractereActuel == EOF)
+            {
+                break;
+            }
+            caractereActuel = fgetc(fichier);
+            // On affiche tous les caractères sauf les '\n' (retour à la ligne)
+            if (caractereActuel != '\n')
+            {
+                printf("%c", caractereActuel);
+            }
+        }
+	}
+	fclose(fichier);
+}
+
+// Algorithme qui compare 2 fichiers ligne par ligne
+void compare(const char *nomDuFichier1, const char *nomDuFichier2)
+{
+	FILE *fichier1 = NULL, *fichier2 = NULL;
+	fichier1 = fopen(nomDuFichier1, "r");
+	fichier2 = fopen(nomDuFichier2, "r");
+	int taille1 = 0, taille2 = 0, i = 0, j = 0, nbLigneFic1 = 1, nbLigneFic2 = 1, tailleTableau = 0;
+    long position1 = 0, position2 = 0;
+
+	if (fichier1 != NULL && fichier2 != NULL)
+	{
+		// Nombre de lignes du fichier 1
+		nbLigneFic1 = compteLigneFichier(nomDuFichier1);
+		// Nombre de lignes du fichier 2
+		nbLigneFic2 = compteLigneFichier(nomDuFichier2);
+
+		for (j=0; j<nbLigneFic1; j++)
+		{
+            // Initialisation
+			char *tabLigne1 = NULL;
+			char *tabLigne2 = NULL;
+			taille1 = 0, taille2 = 0;
+
+            // Taille de caracteres des lignes courantes
+			taille1 = nombreCaractereLigne(nomDuFichier1, j);
+			taille2 = nombreCaractereLigne(nomDuFichier2, j);
+
+			// Taille commune des 2 tableaux
+			if (taille1 > taille2)
+			{
+				tailleTableau = taille1;
+			}
+			else
+			{
+				tailleTableau = taille2;
+			}
+
+            // Allocation d'une mémoire pour les deux tableaux
+			tabLigne1 = malloc(tailleTableau * sizeof(int));
+			tabLigne2 = malloc(tailleTableau * sizeof(int));
+
+			// Retour des curseurs au début de la ligne
+			position1 = retourLigneCurseur(nomDuFichier1, j);
+			fseek(fichier1, position1, SEEK_SET);
+			position2 = retourLigneCurseur(nomDuFichier2, j);
+			fseek(fichier2, position2, SEEK_SET);
+
+            // Ajout de la ligne courante dans le tableau (pour le fichier 1 et le fichier 2)
+			ajouterATableauLigneFichier(nomDuFichier1, tabLigne1, j, tailleTableau);
+			ajouterATableauLigneFichier(nomDuFichier2, tabLigne2, j, tailleTableau);
+
+			int different = 0;
+			for (i = 0; i < tailleTableau; i++)
+			{
+				if (tabLigne1[i] != tabLigne2[i])
+				{
+					different = 1;
+				}
+			}
+			if (different)
+			{
+				// Affichage de ligne fichier 1 et ligne fichier 2
+				afficheLigne(nomDuFichier1, j);
+				printf("\n---\n");
+				afficheLigne(nomDuFichier2, j);
+				printf("\n\n");
+			}
+			free(tabLigne1);
+			free(tabLigne2);
+		}
+		fclose(fichier1);
+		fclose(fichier2);
+	}
+	else
+	{
+		printf("Erreur de lecture du fichier %s ou le fichier %s", nomDuFichier1, nomDuFichier2);
+	}
+}
